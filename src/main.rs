e6cebdcee8d2bf25c6a8b19f4ba5118823567f86
Jason Ng <jng40@stuy.edu>
2024-08-13 15:57:42 -0400

    tried to work on rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use std::process::Command;
+use std::io::stdin;
+fn main(){
+    let mut input = String::new();
+    stdin().read_line(&mut input).unwrap();
+
+    // read_line leaves a trailing newline, which trim removes
+    let command = input.trim(); 
+
+    Command::new(command)
+        .spawn()
+        .unwrap();
 }
+/*
+
+Note to self: 
+    - get documentation of execvp
+    - read the purdue thing of execvp 
+    - read fork documentation 
+    - lexer and pipe 
+    - strtokenizer
+
+*/
