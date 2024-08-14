30c6ff1d2ae3dfd39eb9d61ea82e03f69692d410
Jason Ng <jng40@stuy.edu>
2024-08-14 01:16:53 -0400

    update shell

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,15 @@ fn main(){
     let mut input = String::new();
     stdin().read_line(&mut input).unwrap();
 
+    let mut split = input.trim().split_whitespace();
+    let command = split.next().unwrap();
+    let args = split; // this is an iterator 
     // read_line leaves a trailing newline, which trim removes
-    let command = input.trim(); 
+    // let command = input.trim(); 
 
+    println!("Running command: {}", command);
     Command::new(command)
+        .args(args)
         .spawn()
         .unwrap();
 }
@@ -19,5 +24,8 @@ Note to self:
     - read fork documentation 
     - lexer and pipe 
     - strtokenizer
+
+    let err = exec::execvp("echo", &["echo", "foo"]);
+    println!("Error: {}", err);
 
 */
