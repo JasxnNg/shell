7dd5ff1f76aec9192a1c10c6bed4a73014adf823
Jason Ng <jing06@mit.edu>
2024-12-29 04:35:23 -0500

    figured out piping to a stdout var

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod autocomplete;
 
 
 use color_eyre::Result;
+use nix::libc::fflush;
 use ratatui::{
     crossterm::event::{self, Event, KeyCode, KeyEventKind},
     layout::{Constraint, Layout, Position},
@@ -13,243 +14,244 @@ use ratatui::{
     widgets::{Block, List, ListItem, Paragraph},
     DefaultTerminal, Frame,
 };
+use std::io::{self, Write};
 
-fn main() -> Result<()> {
-    color_eyre::install()?;
-    let terminal = ratatui::init();
-    let words = &mut autocomplete::create_struct();
-    let result = App::new(words).run(terminal);
-    ratatui::restore();
-    result
-}
+// fn main() -> Result<()> {
+//     color_eyre::install()?;
+//     let terminal = ratatui::init();
+//     let words = &mut autocomplete::create_struct();
+//     let result = App::new(words).run(terminal);
+//     ratatui::restore();
+//     result
+// }
 
-// fn run(mut terminal: DefaultTerminal) -> Result<()> {
-//     loop {
-//         terminal.draw(render)?;
-//         if matches!(event::read()?, Event::Key(_)) {
-//             break Ok(());
+// // fn run(mut terminal: DefaultTerminal) -> Result<()> {
+// //     loop {
+// //         terminal.draw(render)?;
+// //         if matches!(event::read()?, Event::Key(_)) {
+// //             break Ok(());
+// //         }
+// //     }
+// // }
+
+// // fn render(frame: &mut Frame) {
+// //     frame.render_widget(misc::path(), frame.area());
+// // }
+
+// /// App holds the state of the application
+// struct App<'a> {
+//     /// Current value of the input box
+//     input: String,
+//     /// Position of cursor in the editor area.
+//     character_index: usize,
+//     /// Current input mode
+//     input_mode: InputMode,
+//     /// History of recorded messages
+//     messages: Vec<String>,
+//     // tree structure for all the words
+//     words: &'a mut autocomplete::Tree<'a>,
+// }
+//     /// Current value of the input bo
+// enum InputMode {
+//     Normal,
+//     Editing,
+// }
+
+// impl <'a> App<'a> {
+//     fn new(word_tree: &'a mut autocomplete::Tree<'a>) -> Self {
+//         Self {
+//             input: String::new(),
+//             input_mode: InputMode::Normal,
+//             messages: Vec::new(),
+//             character_index: 0,
+//             words: word_tree, 
 //         }
 //     }
-// }
 
-// fn render(frame: &mut Frame) {
-//     frame.render_widget(misc::path(), frame.area());
-// }
-
-/// App holds the state of the application
-struct App<'a> {
-    /// Current value of the input box
-    input: String,
-    /// Position of cursor in the editor area.
-    index: usize,
-    character_index: usize,
-    /// Current input mode
-    input_mode: InputMode,
-    /// History of recorded messages
-    messages: Vec<String>,
-    // tree structure for all the words
-    words: &'a mut autocomplete::Tree<'a>,
-}
-    /// Current value of the input bo
-enum InputMode {
-    Normal,
-    Editing,
-}
-
-impl <'a> App<'a> {
-    fn new(word_tree: &'a mut autocomplete::Tree<'a>) -> Self {
-        let index = misc::path().chars().count();
-        Self {
-            input: String::new(),
-            input_mode: InputMode::Normal,
-            messages: Vec::new(),
-            character_index: 0,
-            index,
-            words: word_tree, 
-        }
-    }
-
-    fn move_cursor_left(&mut self) {
-        let cursor_moved_left = self.character_index.saturating_sub(1);
-        self.character_index = self.clamp_cursor(cursor_moved_left);
-    }
-
-    fn move_cursor_right(&mut self) {
-        let cursor_moved_right = self.character_index.saturating_add(1);
-        self.character_index = self.clamp_cursor(cursor_moved_right);
-    }
-
-    fn enter_char(&mut self, new_char: char) {
-        let index = self.byte_index();
-        self.input.insert(index, new_char);
-        self.move_cursor_right();
-    }
-
-    /// Returns the byte index based on the character position.
-    ///
-    /// Since each character in a string can be contain multiple bytes, it's necessary to calculate
-    /// the byte index based on the index of the character.
-    fn byte_index(&self) -> usize {
-        self.input
-            .char_indices()
-            .map(|(i, _)| i)
-            .nth(self.character_index)
-            .unwrap_or(self.input.len())
-    }
-
-    fn delete_char(&mut self) {
-        let is_not_cursor_leftmost = self.character_index != 0;
-        if is_not_cursor_leftmost {
-            // Method "remove" is not used on the saved text for deleting the selected char.
-            // Reason: Using remove on String works on bytes instead of the chars.
-            // Using remove would require special care because of char boundaries.
-
-            let current_index = self.character_index;
-            let from_left_to_current_index = current_index - 1;
-
-            // Getting all characters before the selected character.
-            let before_char_to_delete = self.input.chars().take(from_left_to_current_index);
-            // Getting all characters after selected character.
-            let after_char_to_delete = self.input.chars().skip(current_index);
-
-            // Put all characters together except the selected one.
-            // By leaving the selected one out, it is forgotten and therefore deleted.
-            self.input = before_char_to_delete.chain(after_char_to_delete).collect();
-            self.move_cursor_left();
-        }
-    }
-
-    fn clamp_cursor(&self, new_cursor_pos: usize) -> usize {
-        new_cursor_pos.clamp(misc::path().chars().count(), misc::path().chars().count() + self.input.chars().count())
-    }
-
-    fn reset_cursor(&mut self) {
-        self.character_index = 0;
-    }
-
-    fn submit_message(&mut self) {
-        self.messages.push(self.input.clone());
-        self.input.clear();
-        self.reset_cursor();
-    }
-
-    fn run(mut self, mut terminal: DefaultTerminal) -> Result<()> {
-        loop {
-            terminal.draw(|frame| self.draw(frame))?;
-
-            if let Event::Key(key) = event::read()? {
-                match self.input_mode {
-                    InputMode::Normal => match key.code {
-                        KeyCode::Char('e') => {
-                            self.input_mode = InputMode::Editing;
-                        }
-                        KeyCode::Char('q') => {
-                            return Ok(());
-                        }
-                        _ => {}
-                    },
-                    InputMode::Editing if key.kind == KeyEventKind::Press => match key.code {
-                        KeyCode::Enter => self.submit_message(),
-                        KeyCode::Char(to_insert) => self.enter_char(to_insert),
-                        KeyCode::Backspace => self.delete_char(),
-                        KeyCode::Left => self.move_cursor_left(),
-                        KeyCode::Right => self.move_cursor_right(),
-                        KeyCode::Esc => self.input_mode = InputMode::Normal,
-                        _ => {}
-                    },
-                    InputMode::Editing => {}
-                }
-            }
-        }
-    }
-
-    fn draw(&self, frame: &mut Frame) {
-        let vertical = Layout::vertical([
-            Constraint::Length(1),
-            Constraint::Length(3),
-            Constraint::Min(1),
-        ]);
-        let [help_area, input_area, messages_area] = vertical.areas(frame.area());
-
-        let (msg, style) = match self.input_mode {
-            InputMode::Normal => (
-                vec![
-                    "Press ".into(),
-                    "q".bold(),
-                    " to exit, ".into(),
-                    "e".bold(),
-                    " to start editing.".bold(),
-                ],
-                Style::default().add_modifier(Modifier::RAPID_BLINK),
-            ),
-            InputMode::Editing => (
-                vec![
-                    "Press ".into(),
-                    "Esc".bold(),
-                    " to stop editing, ".into(),
-                    "Enter".bold(),
-                    " to record the message".into(),
-                ],
-                Style::default(),
-            ),
-        };
-        let text = Text::from(Line::from(msg)).patch_style(style);
-        let help_message = Paragraph::new(text);
-        frame.render_widget(help_message, help_area);
-
-        let path = Text::from(misc::path() + self.input.as_str());
-        let input = Paragraph::new(path)
-            .style(match self.input_mode {
-                InputMode::Normal => Style::default(),
-                InputMode::Editing => Style::default().fg(Color::Yellow),
-            })
-            .block(Block::bordered().title("Input"));
-        frame.render_widget(input, input_area);
-        match self.input_mode {
-            // Hide the cursor. `Frame` does this by default, so we don't need to do anything here
-            InputMode::Normal => {}
-
-            // Make the cursor visible and ask ratatui to put it at the specified coordinates after
-            // rendering
-            #[allow(clippy::cast_possible_truncation)]
-            InputMode::Editing => frame.set_cursor_position(Position::new(
-                // Draw the cursor at the current position in the input field.
-                // This position is can be controlled via the left and right arrow key
-                input_area.x + self.character_index as u16 + 1,
-                // Move one line down, from the border to the input line
-                input_area.y + 1,
-            )),
-        }
-
-        let messages: Vec<ListItem> = self
-            .messages
-            .iter()
-            .enumerate()
-            .map(|(i, m)| {
-                let content = Line::from(Span::raw(format!("{i}: {m}")));
-                ListItem::new(content)
-            })
-            .collect();
-        let messages = List::new(messages).block(Block::bordered().title("Messages"));
-        frame.render_widget(messages, messages_area);
-    }
-}
-
-// fn main(){
-//     misc::throwitout();
-//     misc::preamble();
-//     let words = &mut autocomplete::create_struct();
-//     // println!("{}", words);
-//     loop{
-//         misc::path();
-//         let mut input = String::new(); 
-//         io::stdin().read_line(&mut input).unwrap();
-//         // I have to figure out a way 
-//         parser::execute(&input, words);
-        
+//     fn move_cursor_left(&mut self) {
+//         let cursor_moved_left = self.character_index.saturating_sub(1);
+//         self.character_index = self.clamp_cursor(cursor_moved_left);
 //     }
 
+//     fn move_cursor_right(&mut self) {
+//         let cursor_moved_right = self.character_index.saturating_add(1);
+//         self.character_index = self.clamp_cursor(cursor_moved_right);
+//     }
+
+//     fn enter_char(&mut self, new_char: char) {
+//         let index = self.byte_index();
+//         self.input.insert(index, new_char);
+//         self.move_cursor_right();
+//     }
+
+//     /// Returns the byte index based on the character position.
+//     ///
+//     /// Since each character in a string can be contain multiple bytes, it's necessary to calculate
+//     /// the byte index based on the index of the character.
+//     fn byte_index(&self) -> usize {
+//         self.input
+//             .char_indices()
+//             .map(|(i, _)| i)
+//             .nth(self.character_index)
+//             .unwrap_or(self.input.len())
+//     }
+
+//     fn delete_char(&mut self) {
+//         let is_not_cursor_leftmost = self.character_index != 0;
+//         if is_not_cursor_leftmost {
+//             // Method "remove" is not used on the saved text for deleting the selected char.
+//             // Reason: Using remove on String works on bytes instead of the chars.
+//             // Using remove would require special care because of char boundaries.
+
+//             let current_index = self.character_index;
+//             let from_left_to_current_index = current_index - 1;
+
+//             // Getting all characters before the selected character.
+//             let before_char_to_delete = self.input.chars().take(from_left_to_current_index);
+//             // Getting all characters after selected character.
+//             let after_char_to_delete = self.input.chars().skip(current_index);
+
+//             // Put all characters together except the selected one.
+//             // By leaving the selected one out, it is forgotten and therefore deleted.
+//             self.input = before_char_to_delete.chain(after_char_to_delete).collect();
+//             self.move_cursor_left();
+//         }
+//     }
+
+//     fn clamp_cursor(&self, new_cursor_pos: usize) -> usize {
+//         new_cursor_pos.clamp(0, self.input.chars().count())
+//     }
+
+//     fn reset_cursor(&mut self) {
+//         self.character_index = 0;
+//     }
+
+//     fn submit_message(&mut self) {
+
+//         parser::execute(&self.input, self.words);
+//         self.messages.push(self.input.clone());
+//         self.input.clear();
+//         self.reset_cursor();
+//     }
+
+//     fn run(mut self, mut terminal: DefaultTerminal) -> Result<()> {
+//         loop {
+//             terminal.draw(|frame| self.draw(frame))?;
+
+//             if let Event::Key(key) = event::read()? {
+//                 match self.input_mode {
+//                     InputMode::Normal => match key.code {
+//                         KeyCode::Char('e') => {
+//                             self.input_mode = InputMode::Editing;
+//                         }
+//                         KeyCode::Char('q') => {
+//                             return Ok(());
+//                         }
+//                         _ => {}
+//                     },
+//                     InputMode::Editing if key.kind == KeyEventKind::Press => match key.code {
+//                         KeyCode::Enter => self.submit_message(),
+//                         KeyCode::Char(to_insert) => self.enter_char(to_insert),
+//                         KeyCode::Backspace => self.delete_char(),
+//                         KeyCode::Left => self.move_cursor_left(),
+//                         KeyCode::Right => self.move_cursor_right(),
+//                         KeyCode::Esc => self.input_mode = InputMode::Normal,
+//                         _ => {}
+//                     },
+//                     InputMode::Editing => {}
+//                 }
+//             }
+//         }
+//     }
+
+//     fn draw(&self, frame: &mut Frame) {
+//         let vertical = Layout::vertical([
+//             Constraint::Length(1),
+//             Constraint::Length(3),
+//             Constraint::Min(1),
+//         ]);
+//         let [help_area, input_area, messages_area] = vertical.areas(frame.area());
+
+//         let (msg, style) = match self.input_mode {
+//             InputMode::Normal => (
+//                 vec![
+//                     "Press ".into(),
+//                     "q".bold(),
+//                     " to exit, ".into(),
+//                     "e".bold(),
+//                     " to start editing.".bold(),
+//                 ],
+//                 Style::default().add_modifier(Modifier::RAPID_BLINK),
+//             ),
+//             InputMode::Editing => (
+//                 vec![
+//                     "Press ".into(),
+//                     "Esc".bold(),
+//                     " to stop editing, ".into(),
+//                     "Enter".bold(),
+//                     " to record the message".into(),
+//                 ],
+//                 Style::default(),
+//             ),
+//         };
+//         let text = Text::from(Line::from(msg)).patch_style(style);
+//         let help_message = Paragraph::new(text);
+//         frame.render_widget(help_message, help_area);
+
+//         let path = Text::from(misc::path() + self.input.as_str());
+//         let input = Paragraph::new(path)
+//             .style(match self.input_mode {
+//                 InputMode::Normal => Style::default(),
+//                 InputMode::Editing => Style::default().fg(Color::Yellow),
+//             })
+//             .block(Block::bordered().title("Input"));
+//         frame.render_widget(input, input_area);
+//         match self.input_mode {
+//             // Hide the cursor. `Frame` does this by default, so we don't need to do anything here
+//             InputMode::Normal => {}
+
+//             // Make the cursor visible and ask ratatui to put it at the specified coordinates after
+//             // rendering
+//             #[allow(clippy::cast_possible_truncation)]
+//             InputMode::Editing => frame.set_cursor_position(Position::new(
+//                 // Draw the cursor at the current position in the input field.
+//                 // This position is can be controlled via the left and right arrow key
+//                 input_area.x + self.character_index as u16 + 1 + misc::path().len() as u16,
+//                 // Move one line down, from the border to the input line
+//                 input_area.y + 1,
+//             )),
+//         }
+
+//         let messages: Vec<ListItem> = self
+//             .messages
+//             .iter()
+//             .enumerate()
+//             .map(|(i, m)| {
+//                 let content = Line::from(Span::raw(format!("{i}: {m}")));
+//                 ListItem::new(content)
+//             })
+//             .collect();
+//         let messages = List::new(messages).block(Block::bordered().title("Messages"));
+//         frame.render_widget(messages, messages_area);
+//     }
 // }
+
+fn main(){
+    misc::throwitout();
+    misc::preamble();
+    let words = &mut autocomplete::create_struct();
+    // println!("{}", words);
+    loop{
+        print!("{}", misc::path());
+        io::stdout().flush().unwrap();
+        let mut input = String::new(); 
+        io::stdin().read_line(&mut input).unwrap();
+        // I have to figure out a way 
+        parser::execute(&input, words);
+        
+    }
+
+}
 /*
 
 Note to self: 
--- src/parser.rs
+++ src/parser.rs
@@ -6,6 +6,7 @@ Recall that we need to use the declarator "pub" to do something
 use std::process;
 use std::env;
 use std::path::Path;
+use std::process::Stdio;
 
 use crate::autocomplete::Tree;
 use crate::autocomplete;
@@ -147,13 +148,16 @@ pub fn execute (input: &str, autocomplete: &mut Tree) {
 
                 let child = process::Command::new(command)
                 .args(split)
+                .stdout(Stdio::piped())
                 .spawn();
                 
                 match child {
-                    Ok(mut child) => {
+                    Ok(child) => {
                         let childprocessid = child.id();
                         misc::sighandler(childprocessid as i32); 
-                        child.wait().expect("couldn't wait");
+                        let output = child.wait_with_output().expect("couldn't wait");
+                        let stdout = String::from_utf8(output.stdout).unwrap();
+                        print!("{}", stdout);
                     }
                     Err(_error) => {
                         println!("Command not found: {}", command);
